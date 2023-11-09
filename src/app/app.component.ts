80a53c7db0e488936b76aff08b974a29e6a82c0c
Pablo Sastre <pablo.sastre@live.u-tad.com>
2023-11-09 14:11:56 +0100

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PokemonCardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Pokedex';
+  pokemonList = [
+    { name: 'Bulbasaur', type: 'Grass/Poison', imageUrl: 'URL_TO_BULBASAUR_IMAGE' },
+    { name: 'Charmander', type: 'Fire', imageUrl: 'URL_TO_CHARMANDER_IMAGE' },
+    { name: 'Squirtle', type: 'Water', imageUrl: 'URL_TO_SQUIRTLE_IMAGE' }
+  ];
 }
---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pokemon-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pokemon-card.component.html',
+  styleUrl: './pokemon-card.component.css'
+})
+export class PokemonCardComponent {
+    @Input() pokemon: any;
+}
