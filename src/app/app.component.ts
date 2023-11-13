8b232be55b4c2ca64e87a2972577eada0b2ffcbb
Pablo Sastre <pablosastre@live.u-tad.com>
2023-11-13 11:35:30 +0100

    updated stats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,11 @@ import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 export class AppComponent {
   title = 'Pokedex';
   pokemonList = [
-    { name: 'Pikachu', type: './assets/Electric.png', imageUrl: './assets/Pikachu.png', power: 74, damage: 76, attacks: 'Thunder bolt', health: 74, friendly: 78, color1: 'yellow'},
-    { name: 'Rattata', type: './assets/Normal.png', imageUrl: './assets/Rattata.png', power: 74, damage: 76, attacks: 'Drain', health: 74, friendly: 78, color: 'grey'},
-    {name: 'Growlithe', type: './assets/Fire.png', imageUrl: './assets/Growlithe.png', power: 74, damage: 76, attacks: 'Flash fire', health: 74, friendly: 78, color: 'red'},
-    {name: 'Horsea', type: './assets/Water.png', imageUrl: './assets/Horsea.png', power: 74, damage: 76, attacks: 'Sniper', health: 74, friendly: 78, color: 'blue'},
-    {name: 'Clefairy', type: './assets/Fairy.png', imageUrl: './assets/Clefairy.png', power: 74, damage: 76, attacks: 'Magic wall', health: 74, friendly: 78, color: 'pink'},
-    {name: 'Treecko', type: './assets/Grass.png', imageUrl: './assets/Treecko.png', power: 74, damage: 76, attacks: 'Overgrow', health: 74, friendly: 78, color: 'green'}
+    { name: 'Pikachu', type: './assets/Electric.png', imageUrl: './assets/Pikachu.png', power: 74, damage: 76, attacks: 'Thunder bolt', altura: 0.4, peso: 6.0, color1: 'yellow'},
+    { name: 'Rattata', type: './assets/Normal.png', imageUrl: './assets/Rattata.png', power: 74, damage: 76, attacks: 'Drain', altura: 0.3, peso: 3.5, color: 'grey'},
+    {name: 'Growlithe', type: './assets/Fire.png', imageUrl: './assets/Growlithe.png', power: 74, damage: 76, attacks: 'Flash fire', altura: 0.7, peso: 19.0, color: 'red'},
+    {name: 'Horsea', type: './assets/Water.png', imageUrl: './assets/Horsea.png', power: 74, damage: 76, attacks: 'Sniper', altura: 0.4, peso: 8.0, color: 'blue'},
+    {name: 'Clefairy', type: './assets/Fairy.png', imageUrl: './assets/Clefairy.png', power: 74, damage: 76, attacks: 'Magic wall', altura: 0.6, peso: 7.5, color: 'pink'},
+    {name: 'Treecko', type: './assets/Grass.png', imageUrl: './assets/Treecko.png', power: 74, damage: 76, attacks: 'Overgrow', altura: 0.5, peso: 5.0, color: 'green'}
   ];
 }
